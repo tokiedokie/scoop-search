49462351a56a498b3c4ebdf585b74dae2349c258
tokiedokie <42903136+tokiedokie@users.noreply.github.com>
2020-05-09 17:10:19 +0900

    implement input query

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let query = query(env::args()).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        process::exit(1);
+    });
+}
+
+fn query(mut args: env::Args) -> Result<String, &'static str> {
+    args.next();
+    let query = match args.next() {
+        Some(arg) => arg,
+        None => return Err("Didn't get a query"),
+    };
+
+    Ok(query)
 }
