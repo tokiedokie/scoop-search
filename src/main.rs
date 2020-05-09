d540c613911073c1e5b7bd1131c414f6f8ba81d1
tokiedokie <42903136+tokiedokie@users.noreply.github.com>
2020-05-09 17:15:55 +0900

    change fn name get_query

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use std::env;
 use std::process;
 
 fn main() {
-    let query = query(env::args()).unwrap_or_else(|err| {
+    let query = get_query(env::args()).unwrap_or_else(|err| {
         eprintln!("{}", err);
         process::exit(1);
     });
 }
 
-fn query(mut args: env::Args) -> Result<String, &'static str> {
+fn get_query(mut args: env::Args) -> Result<String, &'static str> {
     args.next();
     let query = match args.next() {
         Some(arg) => arg,
